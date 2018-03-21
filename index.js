6378d55d347bb3c60564accf8885ac282785e247
Anawin Vanichyanukroh <Avanichyanukroh@gmail.com>
2018-03-21 03:37:52 -0700

    successfully setup both api, changed css to a basic but more appealing ui for the MVP. Need to incorpate the two api to work together next.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+
+
+
+//TMDB api search code
 const TMDB_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=bd392b20bbcc4afe8b0b8e5db2dc7e99';
 
 function getDataFromApi(searchTerm, callback) {
   
   const settings = {
-    url: TMDB_SEARCH_URL + "&language=en-US" + "&page=1" + "include_adult=false" + "&query=" + searchTerm,
+    url: TMDB_SEARCH_URL + "&language=en-US" + "&page=1" + "&include_adult=false" + "&query=" + searchTerm,
      "async": true,
   "crossDomain": true,
   "method": "GET",
@@ -20,7 +24,12 @@ function getDataFromApi(searchTerm, callback) {
 function renderResult(result) {
   return `
     <div>
-      <h3>${results.title}</h3> <img src="${results.poster_path}"/>
+      <h3>title:${result.title}</h3> <img src="${result.poster_path}"
+      <p>
+      release date: ${result.release_date} <br>
+      rating: ${result.vote_average} <br>
+      popularity: ${result.popularity}
+      
     </div>
   `;
 }
@@ -45,3 +54,60 @@ function watchSubmit() {
 }
 
 $(watchSubmit);
+
+
+
+/*
+//tastedive api search code
+const TASTEDIVE_SEARCH_URL = 'https://tastedive.com/api/similar?k=303566-Numu-0SMN2P46&info=1';
+
+function getDataFromApi(searchTerm, callback) {
+  
+  const settings = {
+    url: TASTEDIVE_SEARCH_URL + "&q=movie:" + searchTerm,
+  type: 'GET',
+  dataType: 'jsonp',
+  success: callback
+  };
+
+  $.ajax(settings).done(function (response) {
+  console.log(response);
+  });
+}
+
+function renderResult(result) {
+  return `
+    <div>
+      <h3>${result.Name}</h3>
+      <p>
+      Teaser: ${result.wTeaser}<br>
+      Wikipedia: ${result.wUrl}<br>
+      Trailer: ${result.yUrl}
+      <p>
+    </div>
+  `;
+}
+
+function displayTasteDiveSearchData(data) {
+  const results = data.Similar.Results.map((item, index) => renderResult(item));
+  console.log(data.Similar.Results);
+  $('.js-search-results').html(results);
+}
+
+
+
+
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(event) {
+      event.preventDefault();
+      const userInput = $(this).find('#js-query');
+      const query =  userInput.val();
+      userInput.val("");
+      getDataFromApi(query, displayTasteDiveSearchData);
+      });
+}
+
+$(watchSubmit);
+
+*/
