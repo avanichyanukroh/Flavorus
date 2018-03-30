197666a4f6170868bfd2683ef5eb4b1331474af7
Anawin Vanichyanukroh <Avanichyanukroh@gmail.com>
2018-03-30 01:52:23 -0700

    finished basic styling for mvp, need to add more info and trailer for each result

--- index.js
+++ index.js
@@ -11,26 +11,40 @@ let exactMovieTitleMap = {};
 function watchSubmit() {
   $('.js-search-form').submit(function(event) {
       event.preventDefault();
+
+      //clears all data collected from previous query if applicable
+      $(".js-search-results").empty();
+      TMDBQueryList = 0;
+      exactMovieTitleList = [];
+      exactMovieTitleMap = {};
+
       const userInput = $(this).find('#js-query');
       const query =  userInput.val();
+
+      $(".suggested-results").html(`
+
+          <h2>Suggested results for "${query}"</h2>
+
+        `);
+
       userInput.val("");
       getDataFromTasteDiveApi(query, queryToTMDBApi);
       });
 }
 
 
-//tastedive api search code
+//tastedive api ajax request
 function getDataFromTasteDiveApi(searchTerm, callback) {
   
   const settings = {
     url: TASTEDIVE_SEARCH_URL + "&q=movie:" + searchTerm,
-  type: 'GET',
-  dataType: 'jsonp',
-  success: callback
-  };
+    type: 'GET',
+    dataType: 'jsonp',
+    success: callback
+    };
 
   $.ajax(settings).done(function (response) {
-  //console.log(response);
+  console.log(response);
   });
 }
 
@@ -44,69 +58,46 @@ function queryToTMDBApi(data) {
       let suggestedMoviesList = data.Similar.Results[i].Name;
 
       suggestedMoviesNameOnly.push(suggestedMoviesList);
-    }
+  }
 
-    //sent to global variable to use the list of suggested titles later in another function
-    TMDBQueryList = suggestedMoviesNameOnly;
+  //sent to global variable to use the list of suggested titles later in another function
+  TMDBQueryList = suggestedMoviesNameOnly;
 
-    for (let i = 0; i < suggestedMoviesNameOnly.length; i ++) {
+  for (let i = 0; i < suggestedMoviesNameOnly.length; i ++) {
 
-      getDataFromTMDBApi(suggestedMoviesNameOnly[i], filterOnlyExactTitle);
+    getDataFromTMDBApi(suggestedMoviesNameOnly[i], filterOnlyExactTitle);
 
-    }
+  }
 
-console.log(exactMovieTitleList);
+//setTimout function to give time for requested data to be received
+  setTimeout(function() {
 
-setTimeout(function() {
+    exactMovieTitleList.sort(function( a , b ) {
 
- exactMovieTitleList.sort(function( a , b ){
-    return a.point_value > b.point_value ? -1 : 1;
- });
+      return a.point_value > b.point_value ? -1 : 1;
 
-  console.log(exactMovieTitleList);
-
-  console.log(exactMovieTitleList.length);
-
-  renderAssortedMovieList(exactMovieTitleList);
-}, 1000);
-
-
-/*
-sort = function( a, b ) {
-
-      let a = a.point_value;
-      let b = b.point_value;
-
-        return(
-            a > b ? 1 :
-            a < b ? -1 :
-            0
-        );
-    };
-
-    //sorts the exactMovieTitleList by point_value from largest to smallest *****ATTENTION, NOT SORTING PROPERLY*****
-    console.log(exactMovieTitleList.sort(sort));
-
+    });
 
     console.log(exactMovieTitleList);
-*/
 
-
-
-
+    renderAssortedMovieList(exactMovieTitleList);
+  
+  }, 1000);
 
 }
 
 
 function renderAssortedMovieList(results) {
 
+    console.log(exactMovieTitleList.length);
+
   for (let i = 0; i < results.length; i ++) {
 
   $(".js-search-results").append(
     `
-    <div class="movieContainer">
-      <img src="https://image.tmdb.org/t/p/w500/${results[i].poster_path}" alt="poster for the movie titled ${results[i].title}" class="poster">
-      <div class="movieContentContainer">
+    <div class="movieContainer row">
+      <img src="https://image.tmdb.org/t/p/w500/${results[i].poster_path}" alt="poster for the movie titled ${results[i].title}" class="poster col-3">
+      <div class="movieContentContainer col-9">
         <h3 class="movieTitle">${results[i].title}</h3>
         <div class="movieReleaseDate">${results[i].release_date}</div>
         <div class="movieRating">${results[i].vote_average}</div>
@@ -133,7 +124,7 @@ function getDataFromTMDBApi(searchTerm, callback) {
     };
 
   $.ajax(settings).done(function (response) {
-  //console.log(response);
+  console.log(response);
   });
 }
 
@@ -154,10 +145,10 @@ function filterOnlyExactTitle(data) {
                 title: data.results[k].title,
                 poster_path: data.results[k].poster_path,
                 release_date: data.results[k].release_date,
-                vote_average: data.results[k].vote_average * 10,
+                vote_average: data.results[k].vote_average * 10 + "%",
                 popularity: data.results[k].popularity,
                 overview: data.results[k].overview,
-                point_value: ((data.results[k].vote_average + data.results[k].popularity) / 2)
+                point_value: (((data.results[k].vote_average * 10) + data.results[k].popularity) / 2)
               };
 
               exactMovieTitleMap[data.results[k].title] = 1;
@@ -174,7 +165,3 @@ function filterOnlyExactTitle(data) {
 
 $(watchSubmit);
 
-
-var customSort = function(a,b) {
-    return [a.a, a.b] > [b.a, b.b] ? 1:-1;
-}
